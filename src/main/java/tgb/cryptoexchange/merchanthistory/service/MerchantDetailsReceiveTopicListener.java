a5eb8fd77cb6a26332c772d81d4378270fc2d29a
barilo <e.barilo@isida.by>
2025-11-18 19:04:59 +0300

    добавлен профиль disable-kafka

--- src/main/java/tgb/cryptoexchange/merchanthistory/service/MerchantDetailsReceiveTopicListener.java
+++ src/main/java/tgb/cryptoexchange/merchanthistory/service/MerchantDetailsReceiveTopicListener.java
@@ -1,10 +1,12 @@
 package tgb.cryptoexchange.merchanthistory.service;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import tgb.cryptoexchange.merchanthistory.dto.MerchantDetailsReceiveEvent;
 
 @Component
+@Profile("!disable-kafka")
 public class MerchantDetailsReceiveTopicListener {
 
     private final MerchantHistoryService merchantHistoryService;
