720b15d090fb347353f7f61d40f7af8f17bc1391
barilo <e.barilo@isida.by>
2025-11-19 18:24:02 +0300

    добавлен RequestMapping

--- src/main/java/tgb/cryptoexchange/merchanthistory/controller/MerchantHistoryController.java
+++ src/main/java/tgb/cryptoexchange/merchanthistory/controller/MerchantHistoryController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tgb.cryptoexchange.controller.ApiController;
 import tgb.cryptoexchange.merchanthistory.dto.MerchantHistoriesResponse;
@@ -14,6 +15,7 @@ import tgb.cryptoexchange.merchanthistory.dto.MerchantHistoryRequest;
 import tgb.cryptoexchange.merchanthistory.service.MerchantHistoryService;
 
 @RestController
+@RequestMapping
 public class MerchantHistoryController extends ApiController {
 
     private final MerchantHistoryService merchantHistoryService;
