1063469e149522c7aeba8d58793fe029512bfee5
barilo <e.barilo@isida.by>
2026-02-06 11:52:13 +0300

    удалена логика статистики

--- src/main/java/tgb/cryptoexchange/merchanthistory/repository/HourDetailsStatisticRepository.java
+++
@@ -1,7 +0,0 @@
-package tgb.cryptoexchange.merchanthistory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tgb.cryptoexchange.merchanthistory.entity.HourDetailsStatistic;
-
-public interface HourDetailsStatisticRepository extends JpaRepository<HourDetailsStatistic, Integer> {
-}
